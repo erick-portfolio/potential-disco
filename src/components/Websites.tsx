7cbfe84a1168e77063a8aa7b5c8dd7a40f6e232b
Erick Gallegos <evgalleg@gmail.com>
2023-03-27 15:04:23 -0400

    removing old sources

--- src/components/Websites.tsx
+++ src/components/Websites.tsx
@@ -5,12 +5,6 @@ export function Websites() {
     <div className="album py-5">
       <div className="row">
         <div className="col-md-4">
-          <RSS url="https://rss.app/feeds/5lxKEhZY0MIWlhNa.xml" title='CBS Sports' />
-        </div>
-        <div className="col-md-4">
-          <RSS url="https://rss.app/feeds/bXj52Mdm2lqsC8Xe.xml" title='Inside Pack Sports' />
-        </div>
-        <div className="col-md-4">
           <RSS url="https://rss.app/feeds/SKuOSGkvoiukNpK5.xml" title='The Wolfpacker Home' />
         </div>
         <div className="col-md-4">
