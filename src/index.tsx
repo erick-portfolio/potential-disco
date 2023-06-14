43bcef7b906b217df8156f7ef8ef0656817bb788
erick <ERock>
2023-06-14 14:02:09 -0400

    adding analytics

--- src/index.tsx
+++ src/index.tsx
@@ -22,16 +22,19 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter basename="/">
-      <App 
-      rssConfig={rssConfig}
-      twitterFeedConfig={twitterFeedConfig}
-      brandText={brandText}
-      disclaimer={disclaimer}/>
+      <App
+        rssConfig={rssConfig}
+        twitterFeedConfig={twitterFeedConfig}
+        brandText={brandText}
+        disclaimer={disclaimer}
+      />
     </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+Analytics.autoTrack('session', {
+  enable: true,
+  provider: 'AWSPinpoint'
+});
+
+reportWebVitals(console.log);
