85034b244080ec253c43528410a9e54123ed596c
erick <ERock>
2023-11-11 12:54:32 -0500

    fixing schedule to display time

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,16 +8,16 @@ export interface FooterProps {
 
 export function Footer ({ isDarkMode, brandText, disclaimer }: FooterProps): React.ReactElement {
   return (
-    <div className={`container${isDarkMode ? ' dark-mode' : ''}`}>
-      <footer className='py-3'>
+    <div className='container'>
+      <footer className={`py-3 ${isDarkMode ? 'dark-mode' : ''}`}>
         <ul className='nav justify-content-center border-bottom pb-3 mb-3'>
           <li className='nav-item'>
-            <a href='/' className='nav-link px-2 text-muted'>
+            <div className='px-2'>
               {brandText}
-            </a>
+            </div>
           </li>
         </ul>
-        <p className='text-center text-muted'>
+        <p className='text-center'>
           {disclaimer}
         </p>
       </footer>
