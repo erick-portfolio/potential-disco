a37247b798c6dd322a4d45eddbceda35fa01f83f
erick <ERock>
2023-11-11 21:12:19 -0500

    more linting and other build fixes

--- src/components/RSSInterfaces.tsx
+++ src/components/RSSInterfaces.tsx
@@ -1,15 +1,14 @@
 // RSSInterfaces.tsx
 export interface RSSItem {
-    title: string
-    link: string
-    pubDate: string
-    author: string
-  }
-  
-  export interface RSSProps {
-    url: string
-    title: string
-    homepage: string
-    isDarkMode: boolean
-  }
-  
+  title: string
+  link: string
+  pubDate: string
+  author: string
+}
+
+export interface RSSProps {
+  url: string
+  title: string
+  homepage: string
+  isDarkMode: boolean
+}
